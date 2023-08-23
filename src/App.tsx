296aa1a82aff6f8231f1513eb53db32ac59d64f1
Arhci <arlan.abkeyev@mail.ru>
2023-08-23 12:06:42 +0300

    add SatCount

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
+import {SatCount} from "./SatCount";
 
 function App() {
 
@@ -24,13 +25,14 @@ function App() {
     const setHandler = () =>{}
 
     return (
+
         <div className="App">
             <div className={count < 10 ? "positive" : "negative" } >{count}</div>
             <div className="button">
                 <button  className="button-inc" onClick={incHandler} disabled={count === 10}>inc</button>
                 <button className="button-reset"  onClick={resetHandler}>reset</button>
-                <button className="button-set"  onClick={setHandler}>set</button>
             </div>
+            <SatCount/>
         </div>
     );
 }
---
+++ src/SatCount.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SatCount.css';
+
+export const SatCount = () => {
+    return (
+        <div className="App2">
+            <input  className="input" onClick={()=>{}} name= "max value:"></input>
+            <input className="input2"  onClick={()=>{}} name="start value:"></input>
+            <div className="button2">
+                <button className="button-set2"  onClick={()=>{}}>set</button>
+            </div>
+        </div>
+    );
+};
